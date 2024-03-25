46adf5008a16ce95c1363defff1bb64f7b064d63
Priyanshu Mishra <priyanshugmishra4001@gmail.com>
2024-03-26 05:01:05 +0530

    Connected to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
-const app = express();
-app.listen(3000 , ()=>{
-    console.log('Server is running on port 3000');
+import mongoose from "mongoose";
 
+mongoose
+  .connect(
+    "mongodb+srv://Priyanshu:priyanshugmishra4001@mern-blog.n1y7dse.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=mern-blog"
+  )
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
