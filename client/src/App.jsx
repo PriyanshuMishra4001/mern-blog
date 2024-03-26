3d3cc68e7f39e1d01c1198ce747c81304487a53f
Priyanshu Mishra <priyanshugmishra4001@gmail.com>
2024-03-26 17:37:49 +0530

    Created the footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Projects from "./pages/Projects";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default function App() {
   return (
     <BrowserRouter>
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
