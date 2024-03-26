4449fb175aea65b1acc888f50705465982ea522a
Priyanshu Mishra <priyanshugmishra4001@gmail.com>
2024-03-26 05:34:34 +0530

    Created a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import userRoutes from './routes/user.route.js';
 mongoose
   .connect(
     "mongodb+srv://Priyanshu:priyanshugmishra4001@mern-blog.n1y7dse.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=mern-blog"
@@ -16,3 +16,6 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+
+app.use('/api/user',userRoutes);
