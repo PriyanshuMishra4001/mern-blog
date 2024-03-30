29fd09f70545432c0eb7e90c5c2893b2fa318c3f
Priyanshu Mishra <priyanshugmishra4001@gmail.com>
2024-03-31 00:43:29 +0530

    Added show more functionality to the post results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -132,7 +132,8 @@ import { Link } from 'react-router-dom';
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
-  console.log(userPosts);
+  const [showMore , setShowMore] = useState(true);
+//   console.log(userPosts);
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -140,6 +141,9 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if(data.posts.length < 9){
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -149,6 +153,24 @@ export default function DashPosts() {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+
+  const handleShowMore = async () =>{
+    const startIndex = userPosts.length;
+    try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+        const data = await res.json();
+        if(res.ok){
+            setUserPosts((prev) => [...prev, ...data.posts]);
+            if(data.posts.length < 9){
+                setShowMore(false);
+            }
+        }        
+    } catch (error) {
+        console.log(error.message)
+    }
+  }
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -205,6 +227,13 @@ export default function DashPosts() {
               </Table.Body>
             ))}
           </Table>
+          {
+            showMore && (
+                <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+                    Show More
+                </button>
+            )
+          }
         </>
       ) : (
         <p>You have no posts yet!</p>
